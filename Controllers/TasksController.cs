6c625da291925c002dc23c21924214fb5c9f11c7
Fabrizio845 <fabrizio.calderon@tecsup.edu.pe>
2025-03-30 02:32:40 -0500

    Corrección en TasksController

--- Controllers/TasksController.cs
+++ Controllers/TasksController.cs
@@ -12,5 +12,19 @@ namespace ToDoApi.Controllers
         // GET /tasks
         [HttpGet]
         public IActionResult GetAll() => Ok(tasks);
+        
+        // POST /tasks
+        [HttpPost]
+        public IActionResult Create([FromBody] ToDo task)
+        {
+            if (string.IsNullOrWhiteSpace(task.Titulo))
+                return BadRequest(new { message = "El título es obligatorio" });
+
+            task.Id = tasks.Count + 1;
+            tasks.Add(task);
+
+            return CreatedAtAction(nameof(GetById), new { id = task.Id }, task);
+        }
+
     }
 }
