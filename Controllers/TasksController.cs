30a980fc427d36b22becfb0e198f3b1025edbeec
Emerson Yaguno <emerson.yaguno@tecsup.edu.pe>
2025-03-30 01:57:49 -0500

    Primer avance de APIRESTfulNet8

---
+++ Controllers/TasksController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDoApi.Models;
+
+namespace ToDoApi.Controllers
+{
+    [ApiController]
+    [Route("api/tasks")]
+    public class TasksController : ControllerBase
+    {
+        private static List<ToDo> tasks = new List<ToDo>();
+
+        // GET /tasks
+        [HttpGet]
+        public IActionResult GetAll() => Ok(tasks);
+    }
+}
---
+++ Models/ToDo.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDoApi.Models
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+        public string? Titulo { get; set; }
+        public string Estado { get; set; } = "Pendiente";
+    }
+}
